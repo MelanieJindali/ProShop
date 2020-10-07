55d770dca8da0b23bec6c4a5edc3a01b89cb588a
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-10-07 09:30:27 -0500

    Add product ratings & description

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,6 +16,21 @@ const ProductScreen = ({ match }) => {
 				<Col md={6}>
 					<Image src={product.image} alt={product.name} fluid />
 				</Col>
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h2>{product.name}</h2>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
 			</Row>
 		</>
 	)
