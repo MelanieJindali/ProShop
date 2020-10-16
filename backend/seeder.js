0d823847549dff65829ea8b3d30280772c8defa3
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-10-15 19:27:45 -0500

    Add function to import data

--- backend/seeder.js
+++ backend/seeder.js
@@ -11,3 +11,26 @@ import connectDB from './config/db.js'
 dotenv.config()
 
 connectDB()
+
+const importData = async () => {
+	try {
+		await Order.deleteMany()
+		await Product.deleteMany()
+		await User.deleteMany()
+
+		const createdUsers = await User.insertMany(users)
+
+		const adminUser = createdUsers[0]._id
+
+		const sampleProducts = products.map((product) => {
+			return { ...product, user: adminUser }
+		})
+
+		await Product.insertMany(sampleProducts)
+		console.log('Data Imported!'.green.inverse)
+		process.exit()
+	} catch (error) {
+		console.log(`${error}`.red.inverse)
+		process.exit(1)
+	}
+}
